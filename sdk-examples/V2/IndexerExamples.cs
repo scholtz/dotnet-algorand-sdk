95e835b9f4e5348803d53a66a2372f68b7f691f3
Wes <ParkGalaxy@live.com>
2022-01-21 16:31:15 -0500

    Update lookupApi examples

--- sdk-examples/V2/IndexerExamples.cs
+++ sdk-examples/V2/IndexerExamples.cs
@@ -24,11 +24,11 @@ namespace sdk_examples.V2
             
             System.Threading.Thread.Sleep(1200); //test in purestake, imit 1 req/sec
             var address = "KV2XGKMXGYJ6PWYQA5374BYIQBL3ONRMSIARPCFCJEAMAHQEVYPB7PL3KU";
-            var acctInfo = await lookupApi.AccountsAsync(address,null,null);
+            var acctInfo = await lookupApi.AccountsAsync(address);
             Console.WriteLine("Look up account by id: " + acctInfo.ToJson());
             
             System.Threading.Thread.Sleep(1200); //test in purestake, imit 1 req/sec
-            var transInfos = await lookupApi.TransactionsGetAsync( 10,null,null,null,null, address,null, null,null,null,null,null,null,null,null,null);
+            var transInfos = await lookupApi.TransactionsGetAsync(address, limit: 10);
             Console.WriteLine("Look up account transactions(limit 10): " + transInfos.ToJson());
 
             System.Threading.Thread.Sleep(1200); //test in purestake, imit 1 req/sec
@@ -39,7 +39,7 @@ namespace sdk_examples.V2
             if (appIndex != null)
             {
                 System.Threading.Thread.Sleep(1200); //test in purestake, imit 1 req/sec
-                var appInfo = await lookupApi.ApplicationsAsync(appIndex.Value, null);
+                var appInfo = await lookupApi.ApplicationsAsync(appIndex.Value);
                 Console.WriteLine("Look up application by id: " + appInfo.ToJson());
             }
 
@@ -50,7 +50,7 @@ namespace sdk_examples.V2
             var assetIndex = assetsInfo.Assets.FirstOrDefault().Index;
            
             System.Threading.Thread.Sleep(1200); //test in purestake, imit 1 req/sec
-            var assetInfo = await lookupApi.AssetsAsync(assetIndex,null);
+            var assetInfo = await lookupApi.AssetsAsync(assetIndex);
             Console.WriteLine("Look up asset by id:" + assetInfo.ToJson());
             
             Console.WriteLine("You have successefully arrived the end of this test, please press and key to exist.");
