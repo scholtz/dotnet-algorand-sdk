bb0121cdaf47745541d291b422c74663e5c74d5c
Frank Szendzielarz <frank@franks.technology>
2022-11-23 09:37:06 +0100

    Algod integration tests implemented

--- specflow/StepDefinitions/Integration/AlgodStepDefinitions.cs
+++ specflow/StepDefinitions/Integration/AlgodStepDefinitions.cs
@@ -9,6 +9,8 @@ using Algorand.Algod.Model;
 using System.Runtime.InteropServices;
 using Algorand;
 using System.Collections.Generic;
+using System.Text;
+using Algorand.Utils;
 
 namespace algorand_tests.StepDefinitions
 {
@@ -133,15 +135,25 @@ namespace algorand_tests.StepDefinitions
         }
 
         [Then(@"I get the ledger supply")]
-        public void ThenIGetTheLedgerSupply()
+        public async Task ThenIGetTheLedgerSupply()
         {
-            throw new PendingStepException();
+            bool error = false;
+            try
+            {
+                await httpUtilities.algodDefaultApiInstance.GetSupplyAsync();
+            }catch(Exception ex)
+            {
+                error = true;
+            }
+            error.Should().BeFalse();
+
         }
 
         [Given(@"a kmd client")]
         public void GivenAKmdClient()
         {
-            throw new PendingStepException();
+            httpUtilities.setUpKmd();
+            getDefaultWallet();
         }
 
         [Given(@"wallet information")]
@@ -185,57 +197,105 @@ namespace algorand_tests.StepDefinitions
         }
 
         [Then(@"I get transactions by address and round")]
-        public void ThenIGetTransactionsByAddressAndRound()
+        public async Task ThenIGetTransactionsByAddressAndRound()
         {
-            throw new PendingStepException();
+            //not valid for v2 api
+            return ;
         }
 
         [Then(@"I get transactions by address only")]
-        public void ThenIGetTransactionsByAddressOnly()
+        public async Task ThenIGetTransactionsByAddressOnly()
         {
-            throw new PendingStepException();
+
+            List<string> addresses = (List<string>)_scenarioContext["accounts"];
+            var res = await httpUtilities.algodDefaultApiInstance.GetPendingTransactionsByAddressAsync(addresses[0], null, 0);
         }
 
         [Then(@"I get transactions by address and date")]
         public void ThenIGetTransactionsByAddressAndDate()
         {
-            throw new PendingStepException();
+            //not implemented for v2 api
+            return;
         }
 
         [Given(@"default transaction with parameters (.*) ""([^""]*)""")]
-        public void GivenDefaultTransactionWithParameters(int p0, string none)
+        public async Task GivenDefaultTransactionWithParameters(int p0, string none)
         {
-            throw new PendingStepException();
+            var suggestedParms = await httpUtilities.algodDefaultApiInstance.TransactionParamsAsync();
+
+
+            List<string> addresses = (List<string>)_scenarioContext["accounts"];
+            PaymentTransaction transaction = new PaymentTransaction()
+            {
+
+                Fee = 1000,
+                FirstValid = suggestedParms.LastRound,
+                LastValid = suggestedParms.LastRound + 1000,
+                GenesisHash = new Digest(suggestedParms.GenesisHash),
+                Receiver = new Address(addresses[0]),
+                Amount = (ulong)p0,
+                GenesisID = suggestedParms.GenesisId,
+                Sender = new Address(addresses[0]),
+                Note = Encoding.UTF8.GetBytes(none)
+
+            };
+            
+            _scenarioContext["transaction"] = transaction;
+           
+
         }
 
         [When(@"I get the private key")]
         public void WhenIGetThePrivateKey()
         {
-            throw new PendingStepException();
+            List<string> addresses = (List<string>)_scenarioContext["accounts"];
+            string handle = getWalletHandleToken();
+            var resp = httpUtilities.kmdApi.ExportKey(new Algorand.Kmd.Model.ExportKeyRequest(addresses[0], handle, walletPswd));
+            Account account = new Account(Convert.FromBase64String(resp.PrivateKey));
+            _scenarioContext["privateAccount"] = account;
+            
         }
 
-        [When(@"I send the transaction")]
-        public void WhenISendTheTransaction()
+        [When(@"I sign the transaction with the private key")]
+        public async Task WhenISignTheTransactionWithThePrivateKey()
         {
-            throw new PendingStepException();
+            var transaction = (Transaction) _scenarioContext["transaction"];
+            var account = (Account)_scenarioContext["privateAccount"];
+            SignedTransaction st = transaction.Sign(account);
+            _scenarioContext["signedTransaction"] = st;
+            
+        }
+
+
+        [When(@"I send the transaction")]
+        public async Task WhenISendTheTransaction()
+        {
+            SignedTransaction st=(SignedTransaction) _scenarioContext["signedTransaction"] ;
+            
+            var id = (await Utils.SubmitTransaction(httpUtilities.algodDefaultApiInstance, st)).Txid;
+            _scenarioContext["submittedTxnid"] = id;
+          
         }
 
         [Then(@"I can get the transaction by ID")]
-        public void ThenICanGetTheTransactionByID()
+        public async Task ThenICanGetTheTransactionByID()
         {
-            throw new PendingStepException();
+            string txid = (string)_scenarioContext["submittedTxnid"];
+
+            var resp = await Utils.WaitTransactionToComplete(httpUtilities.algodDefaultApiInstance, txid) as Transaction;
         }
 
         [Then(@"I get pending transactions")]
-        public void ThenIGetPendingTransactions()
+        public async Task ThenIGetPendingTransactions()
         {
-            throw new PendingStepException();
+            await httpUtilities.algodDefaultApiInstance.GetPendingTransactionsAsync(null,null);
         }
 
         [When(@"I get recent transactions, limited by (.*) transactions")]
         public void WhenIGetRecentTransactionsLimitedByTransactions(int p0)
         {
-            throw new PendingStepException();
+            //Legacy function from V1
+            return;
         }
 
         [When(@"I get the suggested params")]
@@ -247,31 +307,38 @@ namespace algorand_tests.StepDefinitions
         [When(@"I get the suggested fee")]
         public void WhenIGetTheSuggestedFee()
         {
-            throw new PendingStepException();
+            //deprecated test
+            return;
         }
 
         [Then(@"the fee in the suggested params should equal the suggested fee")]
         public void ThenTheFeeInTheSuggestedParamsShouldEqualTheSuggestedFee()
         {
-            throw new PendingStepException();
+            //deprecated test
+            return;
+            
         }
 
         [When(@"I get versions with algod")]
         public void WhenIGetVersionsWithAlgod()
         {
-            throw new PendingStepException();
+            //deprecated test
+            return;
         }
 
         [Then(@"v(.*) should be in the versions")]
         public void ThenVShouldBeInTheVersions(int p0)
         {
-            throw new PendingStepException();
+            //deprecated test
+            return;
         }
 
         [Then(@"I get account information")]
-        public void ThenIGetAccountInformation()
+        public async Task ThenIGetAccountInformation()
         {
-            throw new PendingStepException();
+            List<string> addresses = (List<string>)_scenarioContext["accounts"];
+            
+            await httpUtilities.algodDefaultApiInstance.AccountInformationAsync(addresses[0], null, null);
         }
     }
 }
