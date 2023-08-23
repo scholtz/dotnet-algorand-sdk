f46e752cd62950f670de4f5d1fe447b59821b7df
Frank Szendzielarz <frank@franks.technology>
2023-08-23 12:22:04 +0200

    CompileResponse fix?

--- dotnet-algorand-sdk/Algod/Model/CompileResponse.cs
+++ dotnet-algorand-sdk/Algod/Model/CompileResponse.cs
@@ -1,16 +1,25 @@
 
 namespace Algorand.Algod.Model
 {
-
-using System = global::System;
+    using Newtonsoft.Json.Linq;
+    using System = global::System;
 
 
 public partial class CompileResponse{
 
-  
+        [Newtonsoft.Json.JsonProperty("sourcemap", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+#if UNITY
+    [field:SerializeField]
+    [Tooltip(@"JSON of the source map")]
+    [field:InspectorName(@"Sourcemap")]
+    public byte[] Sourcemap {get;set;}
+#else
+        public JObject Sourcemap { get; set; }
+#endif
 
-    
-}
+
+
+    }
 
 
 }
--- dotnet-algorand-sdk/Algod/Model/CompileResponse.generated.cs
+++ dotnet-algorand-sdk/Algod/Model/CompileResponse.generated.cs
@@ -43,15 +43,6 @@ public partial class CompileResponse{
 
 
 
-    [Newtonsoft.Json.JsonProperty("sourcemap", Required = Newtonsoft.Json.Required.Default,  NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-#if UNITY
-    [field:SerializeField]
-    [Tooltip(@"JSON of the source map")]
-    [field:InspectorName(@"Sourcemap")]
-    public byte[] Sourcemap {get;set;}
-#else
-    public JObject Sourcemap {get;set;}
-#endif
 
 
     
